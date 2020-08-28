2ba1d3f314c8c50f2685efac2b5983344713586d
BrightnessTM <khanolympics@gmail.com>
2020-08-28 11:15:00 -0400

    Emoji fix

--- commands/admin/push.js
+++ commands/admin/push.js
@@ -12,7 +12,7 @@ module.exports = {
 	do: async (message, bot) => {
 		let sent = await bot.createMessage(message.channel.id, "Pushing i18n folder to GitHub...");
 		exec(`cd i18n && git add . && git commit -m "Push new translation changes (${message.author.username})" && git push origin master`).then(async result => {
-			if (bot.getChannel(logs)) await bot.createMessage(logs, `📥 ${message.author.tag} (\`${message.author.id}\`) pushed translations to GitHub`);
+			if (bot.getChannel(logs)) await bot.createMessage(logs, `:outbox_tray: ${message.author.tag} (\`${message.author.id}\`) pushed translations to GitHub`);
 			return sent.edit(`\`\`\`xl\n${result.stdout.substr(0, 1900)}\n\`\`\``);
 		}).catch(err => sent.edit(`Error: \`${err.stderr}\``));
 	}
